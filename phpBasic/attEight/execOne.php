6c33ac99b2f60de5e60f7b764e2b012d2b226c17
rodriguessavio <capar2022113TADS0049@aluno.ifpi.edu.br>
2023-09-19 22:04:02 -0300

    explode adc

--- phpBasic/attEight/execOne.php
+++ phpBasic/attEight/execOne.php
@@ -1,1 +1,27 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method= "post">
+        <label for="">Entre com uma lista de numeros separados por vígula para serem somados: </label>
+        <input type="text" name = "value">
+        <br>
+        <input type="submit" value="Enviar">
+    </form>
+    <?php
+        $value=@$_POST['value'];
+        $vetor = explode(", ", $value );
+        $result = 0;
+        foreach ($vetor as $num) {
+            $result += (float)$num;
+        }
+
+        echo "<h4>Resultado: $result</h4>";
+
+    ?>
+</body>
+</html>
